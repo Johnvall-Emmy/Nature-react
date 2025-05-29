4272436881ff9afacfb44e70926dc28c52554660
Johnvall-Emmy <emmanuelchu78@gmail.com>
2025-05-29 14:34:34 +0100

    adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
-import Hero from "./Components/Hero/Hero";
-import Main from "./Components/Main-section/Main";
-import Service from "./Components/Service folder/Service";
-import Aboutus from "./Components/Aboutus folder/Aboutus";
-import Question from "./Components/Questionsection/Question";
-import Footer from "./Components/Footer Folder/Footer";
+// import Navbar from "./Components/Navbar/Navbar";
+// import Hero from "./Components/Hero/Hero";
+// import Main from "./Components/Main-section/Main";
+// import Service from "./Components/Service folder/Service";
+// import Aboutus from "./Components/Aboutus folder/Aboutus";
+// import Question from "./Components/Questionsection/Question";
+// import Footer from "./Components/Footer Folder/Footer";
 
 function App() {
   return (
     <>
       <div>
-        <Navbar />
+        hi
+        {/* <Navbar />
         <Hero />
         <Main />
         <Service />
         <Aboutus />
         <Question />
-        <Footer />
+        <Footer /> */}
       </div>
     </>
   );
