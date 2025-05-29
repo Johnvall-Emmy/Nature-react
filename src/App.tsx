6fc4c25244c65c6bc6102e094267a7cb0e9cb724
Johnvall-Emmy <emmanuelchu78@gmail.com>
2025-05-29 14:09:40 +0100

    adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
@@ -11,8 +8,6 @@ import Question from "./Components/Questionsection/Question";
 import Footer from "./Components/Footer Folder/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
